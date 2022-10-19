14235bf69dac978f5043ef35ad720805a069947c
Joshua Lee <joshua.putra@ti.ukdw.ac.id>
2022-10-19 15:28:45 +0700

    about page & select box on homepage
    TODO = formlist component to input data

---
+++ pages/About.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+st.title("Tentang programs")
+st.write("dikembangkan sebagai implementasi metodologi skripsi")
+st.write("Joshua Putra Setyadi (71170173)")
+st.write("Universitas Kristen Duta Wacana")
