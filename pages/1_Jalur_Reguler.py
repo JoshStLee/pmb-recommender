87eeb4600c4d965a60883864407659c9ed4b2aac
Joshua Lee <joshualie1999@gmail.com>
2022-11-14 23:53:29 +0700

    added session state to store school list and new entries
    i need to find a way to reset the text input

--- pages/1_Jalur_Reguler.py
+++ pages/1_Jalur_Reguler.py
@@ -12,7 +12,22 @@ st.set_page_config(
     initial_sidebar_state = "collapsed"
 )
 
-daftar_sekolah = pd.read_csv('daftar_sekolah.csv')[['id_daftar_sekolah','sekolah_asal','tipe_sekolah_asal']]
+if "daftar_sekolah" not in st.session_state :
+    st.session_state.daftar_sekolah = pd.read_csv('daftar_sekolah.csv')[['id_daftar_sekolah',
+                                                                         'sekolah_asal',
+                                                                         'tipe_sekolah_asal']]
+if "data_baru_reguler" not in st.session_state:
+    st.session_state.data_baru_reguler = pd.DataFrame(columns=['lokasi','status','tipe',
+                                                       'id_daftar_sekolah',
+                                                       'id_prodi_pilihan_1',
+                                                       'id_prodi_pilihan_2',
+                                                       'nilai_tpa_verbal',
+                                                       'nilai_tpa_spasial',
+                                                       'nilai_tpa_analogi',
+                                                       'nilai_tpa_numerik',
+                                                       'is_diterima'])
+    
+daftar_sekolah = st.session_state.daftar_sekolah
 daftar_provinsi = pd.read_csv('daftar_provinsi.csv')
 daftar_prodi = pd.read_csv('daftar_prodi.csv')
 
@@ -20,7 +35,7 @@ regular_model =  pickle.load(open('regular_entry_model.sav', 'rb'))
 
 st.title("Rekomendasi Pemilihan Mahasiswa Baru Jalur Reguler")
 
-with st.form("my_form"):
+with st.form("my_form", clear_on_submit=True):
     kode_pendaftar = st.text_input("Kode Pendaftar","")
     provinsi_asal = st.selectbox("Provinsi Asal", daftar_provinsi[['provinsi']])
     container_for_selectbox = st.empty()
@@ -36,43 +51,32 @@ with st.form("my_form"):
             
     submitted = st.form_submit_button("Submit")
     container_when_submitted = st.empty()
-    table_when_submitted = st.empty()
-
-# def convert_to_school_id(sekolah_asal):
-            
-with container_for_selectbox:
-    sekolah_asal = st.selectbox("Sekolah Asal",daftar_sekolah[['sekolah_asal']])    
-    id_daftar_sekolah = daftar_sekolah.loc[daftar_sekolah['sekolah_asal']==sekolah_asal, 'id_daftar_sekolah'].iloc[0]    
-with container_for_optional_text:
-    if sekolah_asal == "TIDAK TERDAFTAR": 
-        sekolah_asal = st.text_input("Masukkan nama sekolah")       
-        if submitted:
-            df2 = pd.DataFrame([[daftar_sekolah.index[-1]+1,sekolah_asal,status_sekolah]], 
-                               columns=['id_daftar_sekolah','sekolah_asal','tipe_sekolah_asal'])
-            daftar_sekolah = pd.concat([daftar_sekolah,df2])
-            id_daftar_sekolah = daftar_sekolah.loc[daftar_sekolah['sekolah_asal']==sekolah_asal, 'id_daftar_sekolah'].iloc[0]
-with container_when_submitted.container():     
     id_prodi_pilihan_1 = daftar_prodi.loc[daftar_prodi['nama_prodi']==prodi_pilihan_1, 'id_prodi'].iloc[0]
     id_prodi_pilihan_2 = daftar_prodi.loc[daftar_prodi['nama_prodi']==prodi_pilihan_2, 'id_prodi'].iloc[0]
     id_provinsi = daftar_provinsi.loc[daftar_provinsi['provinsi']==provinsi_asal, 'id_provinsi'].iloc[0]
     lokasi = 1 if id_provinsi>12 & id_provinsi<19 else 0
     tipe = 0 if jurusan_sekolah == 'Lainnya' else 1 
     status = 1 if status_sekolah =="NEGERI" else 0
-    
-    # if submitted:    
-    #     st.write("Provinsi asal", provinsi_asal)
-    #     st.write("Sekolah asal", sekolah_asal)
-    #     st.write("Jurusan sekolah", jurusan_sekolah)
-    #     st.write("Status Sekolah", status_sekolah)
-    #     st.write("Pilihan Prodi Pertama", prodi_pilihan_1)
-    #     st.write("Pilihan Prodi Kedua", prodi_pilihan_2)
-    #     st.write("Nilai TPA Verbal", nilai_tpa_verbal)
-    #     st.write("Nilai TPA Spasial", nilai_tpa_spasial)
-    #     st.write("Nilai TPA Analogi", nilai_tpa_analogi)
-    #     st.write("Nilai TPA Numerik", nilai_tpa_numerik)
+
+with container_for_selectbox:
+    sekolah_asal = st.selectbox("Sekolah Asal",daftar_sekolah[['sekolah_asal']])    
+    id_daftar_sekolah = daftar_sekolah.loc[daftar_sekolah['sekolah_asal']==sekolah_asal, 
+                                               'id_daftar_sekolah'].iloc[0]    
+
+with container_for_optional_text:
+    if sekolah_asal == "TIDAK TERDAFTAR": 
+        sekolah_asal = st.text_input("Masukkan nama sekolah")       
+        if submitted:
+            df = pd.DataFrame([[daftar_sekolah.index[-1]+1,sekolah_asal,status_sekolah]], 
+                              columns=['id_daftar_sekolah','sekolah_asal','tipe_sekolah_asal'])
+            daftar_sekolah = pd.concat([daftar_sekolah,df], ignore_index=True)
+            id_daftar_sekolah = daftar_sekolah.loc[daftar_sekolah['sekolah_asal']==sekolah_asal, 
+                                                   'id_daftar_sekolah'].iloc[0]
+            st.session_state.daftar_sekolah = daftar_sekolah
+                #session state now saves the new school list
         
-        
-test = {
+with container_when_submitted.container():       
+    test = pd.DataFrame({
         'lokasi':[lokasi],
         'status':[status],
         'tipe':[tipe],
@@ -83,13 +87,35 @@ test = {
         'nilai_tpa_spasial':[nilai_tpa_spasial],
         'nilai_tpa_analogi':[nilai_tpa_analogi],
         'nilai_tpa_numerik':[nilai_tpa_numerik]
-       }
+       })
 
-dummy = pd.DataFrame(test)
-
-with table_when_submitted.container():
     if submitted:
-        result = regular_model.predict(dummy)[0]
+        result = regular_model.predict(test)[0]
         text_result = "DITERIMA" if result == 1 else "DITOLAK"
-        st.write("HASIL ML", result, text_result)
-        st.table(dummy)
+        st.write("HASIL ML", text_result)
+        
+        # commented because it stores the values now
+        # st.dataframe(daftar_sekolah)
+        
+        # commented because it stores the values now
+        # st.dataframe(st.session_state.data_baru_reguler)
+        entry = pd.DataFrame({
+            'lokasi':[lokasi],
+            'status':[status],
+            'tipe':[tipe],
+            'id_daftar_sekolah':[id_daftar_sekolah],
+            'id_prodi_pilihan_1':[id_prodi_pilihan_1],
+            'id_prodi_pilihan_2':[id_prodi_pilihan_2],
+            'nilai_tpa_verbal':[nilai_tpa_verbal],
+            'nilai_tpa_spasial':[nilai_tpa_spasial],
+            'nilai_tpa_analogi':[nilai_tpa_analogi],
+            'nilai_tpa_numerik':[nilai_tpa_numerik],
+            'is_diterima':[result]
+        })
+        
+        st.session_state.data_baru_reguler = pd.concat([st.session_state.data_baru_reguler, 
+                                                        entry], ignore_index=True) 
+        st.experimental_rerun()
+        
+        # TODO RETRAIN THE ML
+        # if st.write(st.session_state.data_baru_reguler.index[-1]) == 5:
