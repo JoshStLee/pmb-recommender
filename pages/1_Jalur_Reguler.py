76d86948edd9515ab041e8d0666fc94850c2cea3
Joshua Lee <joshualie1999@gmail.com>
2022-11-15 15:53:34 +0700

    retrained ai when received new data, but with the ML-generated class
    wait for next pointer

--- pages/1_Jalur_Reguler.py
+++ pages/1_Jalur_Reguler.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import sklearn
+from sklearn.ensemble import RandomForestClassifier
 from PIL import Image
+from sklearn.model_selection import train_test_split
 icon = Image.open('icon_ukdw.png')
 
 st.set_page_config(
@@ -16,6 +17,7 @@ if "daftar_sekolah" not in st.session_state :
     st.session_state.daftar_sekolah = pd.read_csv('daftar_sekolah.csv')[['id_daftar_sekolah',
                                                                          'sekolah_asal',
                                                                          'tipe_sekolah_asal']]
+    
 if "data_baru_reguler" not in st.session_state:
     st.session_state.data_baru_reguler = pd.DataFrame(columns=['lokasi','status','tipe',
                                                        'id_daftar_sekolah',
@@ -30,8 +32,20 @@ if "data_baru_reguler" not in st.session_state:
 daftar_sekolah = st.session_state.daftar_sekolah
 daftar_provinsi = pd.read_csv('daftar_provinsi.csv')
 daftar_prodi = pd.read_csv('daftar_prodi.csv')
-
+regular_model = RandomForestClassifier(n_estimators=10)
 regular_model =  pickle.load(open('regular_entry_model.sav', 'rb'))
+njp = pd.read_csv('data_njp.csv')
+y = njp[['is_diterima']]
+X = njp.drop(['is_diterima'], axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4, random_state = 42)
+
+def retrain_ml(X_train, y_train, regular_model):
+    train_baru = st.session_state.data_baru_reguler.drop(['is_diterima'],axis=1)
+    class_baru = st.session_state.data_baru_reguler[['is_diterima']]
+    X_train = pd.concat([X_train,train_baru], ignore_index=True)
+    y_train = pd.concat([y_train,class_baru], ignore_index=True)
+    regular_model.fit(X_train.astype(int), y_train.astype(int))
+    st.session_state.data_baru_reguler.drop(st.session_state.data_baru_reguler.index, inplace=True)  
 
 st.title("Rekomendasi Pemilihan Mahasiswa Baru Jalur Reguler")
 
@@ -57,9 +71,9 @@ with st.form("my_form", clear_on_submit=True):
     lokasi = 1 if id_provinsi>12 & id_provinsi<19 else 0
     tipe = 0 if jurusan_sekolah == 'Lainnya' else 1 
     status = 1 if status_sekolah =="NEGERI" else 0
-
+    
 with container_for_selectbox:
-    sekolah_asal = st.selectbox("Sekolah Asal",daftar_sekolah[['sekolah_asal']])    
+    sekolah_asal = st.selectbox("Sekolah Asal",daftar_sekolah[['sekolah_asal']])   
     id_daftar_sekolah = daftar_sekolah.loc[daftar_sekolah['sekolah_asal']==sekolah_asal, 
                                                'id_daftar_sekolah'].iloc[0]    
 
@@ -94,11 +108,6 @@ with container_when_submitted.container():
         text_result = "DITERIMA" if result == 1 else "DITOLAK"
         st.write("HASIL ML", text_result)
         
-        # commented because it stores the values now
-        # st.dataframe(daftar_sekolah)
-        
-        # commented because it stores the values now
-        # st.dataframe(st.session_state.data_baru_reguler)
         entry = pd.DataFrame({
             'lokasi':[lokasi],
             'status':[status],
@@ -115,7 +124,24 @@ with container_when_submitted.container():
         
         st.session_state.data_baru_reguler = pd.concat([st.session_state.data_baru_reguler, 
                                                         entry], ignore_index=True) 
-        st.experimental_rerun()
+        with container_for_selectbox:
+            sekolah_asal = st.selectbox("Sekolah Asal",daftar_sekolah[['sekolah_asal']])   
+            id_daftar_sekolah = daftar_sekolah.loc[daftar_sekolah['sekolah_asal']==sekolah_asal, 
+                                                       'id_daftar_sekolah'].iloc[0]    
+        with container_for_optional_text:
+            if sekolah_asal == "TIDAK TERDAFTAR": 
+                sekolah_asal = st.text_input("Masukkan nama sekolah")       
+                if submitted:
+                    df = pd.DataFrame([[daftar_sekolah.index[-1]+1,sekolah_asal,status_sekolah]], 
+                                      columns=['id_daftar_sekolah','sekolah_asal','tipe_sekolah_asal'])
+                    daftar_sekolah = pd.concat([daftar_sekolah,df], ignore_index=True)
+                    id_daftar_sekolah = daftar_sekolah.loc[daftar_sekolah['sekolah_asal']==sekolah_asal, 
+                                                           'id_daftar_sekolah'].iloc[0]
+                    st.session_state.daftar_sekolah = daftar_sekolah
+                        #session state now saves the new school list
         
-        # TODO RETRAIN THE ML
-        # if st.write(st.session_state.data_baru_reguler.index[-1]) == 5:
+if st.session_state.data_baru_reguler.shape[0]  >= 5:
+    if st.button('latih ulang ML'):
+        retrain_ml(X_train, y_train, regular_model)
+        
+            
